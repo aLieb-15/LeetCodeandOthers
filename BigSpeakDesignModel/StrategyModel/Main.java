e3465ce5b0d0a78b6905b371e784ef40cb5ccde7
aLieb-15 <alieb@bupt.edu.cn>
2023-10-10 14:51:45 +0800

    添加对策略模式的总结

--- BigSpeakDesignModel/StrategyModel/Main.java
+++ BigSpeakDesignModel/StrategyModel/Main.java
@@ -1,5 +1,12 @@
 package BigSpeakDesignModel.StrategyModel;
 
+/**
+ * 策略模式定义一系列算法，这些算法完成相同的工作，只是实现不同
+ * 算法的调用方式都是一样的
+ * Strategy类层次为Context定义了一系列可供重用的算法和行为
+ * 这有助于析取所有算法的公共功能
+ * 并且有助于简化单元测试，每个算法都有自己的类，可以通过自己的接口进行单独测试
+ */
 public class Main {
     private static Double total = 0.0d;
 
